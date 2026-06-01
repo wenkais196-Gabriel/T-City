bdac3e483dd35322786baa83116c852b6fbf5786
Song Wenkai <wenkais196@gmail.com>
2026-06-01 10:53:48 +0200

    fix: 添加 txAdminPort 0 禁用管理员模式

--- start_server.bat
+++ start_server.bat
@@ -6,5 +6,5 @@ echo   T-City Lite 开发服务器
 echo   当前分支: v0.5-crime
 echo ========================================
 echo.
-D:\server\FXServer.exe +exec T-CityLite.base/server.cfg
+D:\server\FXServer.exe +set txAdminPort 0 +exec T-CityLite.base/server.cfg
 pause
